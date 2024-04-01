bb6b431c458226a2a502b03015f9255cf36e55d6
mohammed <mhw9898@gmail.com>
2024-04-01 11:20:14 +0300

    Add files via upload
    oracle dump file

---
+++ ImportDB.bat
@@ -0,0 +1,2 @@
+imp user/password FULL=y FILE=E:VOTING
+GRANTS=y ROWS=y
---
+++ exportDB.bat
@@ -0,0 +1,3 @@
+start D:\WINDOWS.X64_193000_db_home\aa\bin\exp USER/Password@ServiceName FULL=n FILE=D:\backup\filename.dmp 
+GRANTS=y ROWS=y 
+exit
